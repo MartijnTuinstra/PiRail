5ddbeb12aca92388aa4672a49197a75e1dbde9da
MartijnTuinstra <md.tuinstra@gmail.com>
2018-09-05 23:22:13 +0200

    Updated the train simulation

--- src/train_sim.c
+++ src/train_sim.c
@@ -11,11 +11,32 @@ pthread_mutex_t mutex_lockA;
 
 #define delayA 5000000
 #define delayB 5000000
+#define OneSec 1000000
 
+#define TRAIN_A_LEN   30 //cm
+#define TRAIN_A_SPEED 10 //cm/s
+
+
+void change_Block(Block * B, enum Rail_states state){
+  B->changed |= IO_Changed;
+  B->state = state;
+  if (state == BLOCKED)
+    B->blocked = 1;
+  else
+    B->blocked = 0;
+  process(B, 2);
+  if(B->changed & IO_Changed){
+    loggerf(INFO, "ReProcess");
+    process(B, 2);
+  }
+  WS_trackUpdate(0);
+  WS_SwitchesUpdate(0);
+}
 
 void *TRAIN_SIMA(){
   Block *B = Units[20]->B[10];
   Block *N = Units[20]->B[10];
+  Block *N2 = 0;
 
   B->state = BLOCKED;
   B->blocked = 1;
@@ -36,9 +57,6 @@ void *TRAIN_SIMA(){
 
 
   while(_SYS->_STATE & STATE_RUN){
-    loggerf(MEMORY, "Train Sim Step (id:%i)\n",pthread_self());
-
-    pthread_mutex_lock(&mutex_lockA);
 
     N = Next(B, NEXT, 1);
     if(!N){
@@ -47,188 +65,117 @@ void *TRAIN_SIMA(){
         usleep(100000);
       }
     }
-    loggerf(TRACE, "Sim A step to %i:%i",N->module,N->id);
-    N->changed |= IO_Changed;
-    N->state = BLOCKED;
-    N->blocked = 1;
-    process(N, 2);
-    if(N->changed & IO_Changed){
-      loggerf(INFO, "ReProcess");
-      process(N, 2);
-    }
-    WS_trackUpdate(0);
-    WS_SwitchesUpdate(0);
+    loggerf(DEBUG, "Sim B step %i:%i", N->module, N->id);
+    change_Block(N, BLOCKED);
 
-    pthread_mutex_unlock(&mutex_lockA);
-    usleep(delayA/2);
-    pthread_mutex_lock(&mutex_lockA);
-    B->changed |= IO_Changed;
-    B->blocked = 0;
-    B->state = PROCEED;
-    process(B, 2);
-    if(B->changed & IO_Changed){
-      loggerf(INFO, "ReProcess");
-      process(B, 2);
-    }
+    // IF len(N) < len(TRAIN)
+    if(N->length < TRAIN_A_LEN){
+      usleep((N->length/TRAIN_A_SPEED) * OneSec);
+      N2 = Next(B, NEXT, 2);
+      if(!N2){
+        loggerf(WARNING, "Sim A reached end of the line");
+        while(1){
+          usleep(100000);
+        }
+      }
+      loggerf(DEBUG, "Sim A substep %i:%i", N2->module, N2->id);
 
-    WS_trackUpdate(0);
-    WS_SwitchesUpdate(0);
-    
-    pthread_mutex_unlock(&mutex_lockA);
-    usleep(delayA/2);
-    pthread_mutex_lock(&mutex_lockA);
-    B = N;
-    pthread_mutex_unlock(&mutex_lockA);
+      change_Block(N2, BLOCKED);
+      usleep(((TRAIN_A_LEN - N->length)/TRAIN_A_SPEED) * OneSec);
+      change_Block(B, PROCEED);
+      if(N2 && N2->length > TRAIN_A_LEN){
+        usleep(((N2->length - (TRAIN_A_LEN - N->length))/TRAIN_A_SPEED) * OneSec);
+        change_Block(N, PROCEED);
+        usleep(((N2->length - TRAIN_A_LEN)/TRAIN_A_SPEED) * OneSec);
+
+        B = N2;
+      }
+      else{
+        loggerf(CRITICAL, "Two short blocks smaller than train A");
+      }
+    }
+    else{
+      usleep((TRAIN_A_LEN/TRAIN_A_SPEED) * OneSec);
+      change_Block(B, PROCEED);
+      usleep(((N->length - TRAIN_A_LEN)/TRAIN_A_SPEED) * OneSec);
+
+      B = N;
+    }
   }
 
   return 0;
 }
+
 
 void *TRAIN_SIMB(){
-  Block *B = Units[4]->B[23];
-  Block *N = Units[4]->B[23];
-  Block *A = 0;
-  int i = 0;
+  Block *B = Units[20]->B[5];
+  Block *N = Units[20]->B[5];
+  Block *N2 = 0;
 
   B->state = BLOCKED;
+  B->blocked = 1;
   B->changed  = 1;
+  process(B, 2);
+  WS_trackUpdate(0);
+  WS_SwitchesUpdate(0);
 
-  while(B->train == 0){}
+  while(1){
+    if(B->state == RESTRICTED){ //B->train != 0 && train_link[B->train] != 0 || 
+      break;
+    }
+    else if((_SYS->_STATE & STATE_RUN) == 0){
+      break;
+    }
+    usleep(100);
+  }
 
-  while(!train_link[B->train]){}
-
-  train_link[B->train]->halt = 1;
-
-  while(train_link[B->train]->halt == 1){}
 
   while(_SYS->_STATE & STATE_RUN){
-    //printf("Train Sim Step (id:%i)\n",pthread_self());
-    while(1){
-      if(train_link[2] && train_link[2]->halt == 0){
-        break;
-      }
-      usleep(1000);
-    }
 
-    pthread_mutex_lock(&mutex_lockA);
-
-    N = Next(B,0,1+i);
-    if(i > 0){
-      A = Next(B,0,i);
-    }
+    N = Next(B, NEXT, 1);
     if(!N){
+      loggerf(WARNING, "Sim B reached end of the line");
       while(1){
         usleep(100000);
       }
     }
-    //printf(" %i:%i:%i\n",N->Adr.M,N->Adr.B,N->Adr.S);
-    N->changed = 1;
-    N->state = BLOCKED;
-    pthread_mutex_unlock(&mutex_lockA);
-    usleep(delayA/2);
-    pthread_mutex_lock(&mutex_lockA);
-    if(i>0){
-      A->changed = 1;
-      A->state = DANGER;
-    }else{
-      B->changed = 1;
-      B->state = DANGER;
+    loggerf(DEBUG, "Sim B step %i:%i", N->module, N->id);
+    change_Block(N, BLOCKED);
+
+    // IF len(N) < len(TRAIN)
+    if(N->length < TRAIN_A_LEN){
+      usleep((N->length/TRAIN_A_SPEED) * OneSec);
+      N2 = Next(B, NEXT, 2);
+      if(!N2){
+        loggerf(WARNING, "Sim B reached end of the line");
+        while(1){
+          usleep(100000);
+        }
+      }
+      loggerf(DEBUG, "Sim B substep %i:%i", N2->module, N2->id);
+
+      change_Block(N2, BLOCKED);
+      usleep(((TRAIN_A_LEN - N->length)/TRAIN_A_SPEED) * OneSec);
+      change_Block(B, PROCEED);
+      if(N2 && N2->length > TRAIN_A_LEN){
+        usleep(((N2->length - (TRAIN_A_LEN - N->length))/TRAIN_A_SPEED) * OneSec);
+        change_Block(N, PROCEED);
+        usleep(((N2->length - TRAIN_A_LEN)/TRAIN_A_SPEED) * OneSec);
+
+        B = N2;
+      }
+      else{
+        loggerf(CRITICAL, "Two short blocks smaller than train B");
+      }
     }
-    pthread_mutex_unlock(&mutex_lockA);
-    usleep(delayA/2);
-    pthread_mutex_lock(&mutex_lockA);
-    if(N->type == 'T'){
-      i++;
-    }else{
+    else{
+      usleep((TRAIN_A_LEN/TRAIN_A_SPEED) * OneSec);
+      change_Block(B, PROCEED);
+      usleep(((N->length - TRAIN_A_LEN)/TRAIN_A_SPEED) * OneSec);
+
       B = N;
-      i = 0;
     }
-    pthread_mutex_unlock(&mutex_lockA);
   }
 
   return 0;
 }
-/*
-void *TRAIN_SIMC(){
-  struct Seg *B2 = blocks2[7][5];
-  struct Seg *N2 = blocks2[7][5];
-  struct Seg *A2[3] = {blocks2[0][0]};
-  int i2 = 0;
-  pthread_mutex_lock(&mutex_lockA);
-  while(_SYS->_STATE & STATE_RUN){
-    //printf("Train Sim Step (id:%i)\t",pthread_self());
-    N2 = Next2(B2,1+i2);
-    if(i2 > 0){
-      A2[i2] = Next2(B2,i2);
-    }
-    if(!N2){
-      while(1){
-        usleep(100000);
-      }
-    }
-    //printf(" %i:%i:%i\n",N2->Adr.M,N2->Adr.B,N2->Adr.S);
-    N2->change = 1;
-    N2->blocked = 1;
-    pthread_mutex_unlock(&mutex_lockA);
-    usleep(delayB/2);
-    pthread_mutex_lock(&mutex_lockA);
-    if(i2>0){
-      A2[i2]->change = 1;
-      A2[i2]->blocked = 0;
-    }else{
-      B2->change = 1;
-      B2->blocked = 0;
-    }
-    pthread_mutex_unlock(&mutex_lockA);
-    usleep(delayB/2);
-    pthread_mutex_lock(&mutex_lockA);
-    if(N2->Adr.S == 0){
-      i2++;
-    }else{
-      B2 = N2;
-      i2 = 0;
-    }
-  }
-}
-void *TRAIN_SIMD(){
-  struct Seg *B2 = blocks2[7][2];
-  struct Seg *N2 = blocks2[7][2];
-  struct Seg *A2[3] = {blocks2[0][0]};
-  int i2 = 0;
-  pthread_mutex_lock(&mutex_lockA);
-  while(_SYS->_STATE & STATE_RUN){
-    //printf("Train Sim Step (id:%i)\t",pthread_self());
-    N2 = Next2(B2,1+i2);
-    if(i2 > 0){
-      A2[i2] = Next2(B2,i2);
-    }
-    if(!N2){
-      while(1){
-        usleep(100000);
-      }
-    }
-    //printf(" %i:%i:%i\n",N2->Adr.M,N2->Adr.B,N2->Adr.S);
-    N2->change = 1;
-    N2->blocked = 1;
-    pthread_mutex_unlock(&mutex_lockA);
-    usleep(delayB/2);
-    pthread_mutex_lock(&mutex_lockA);
-    if(i2>0){
-      A2[i2]->change = 1;
-      A2[i2]->blocked = 0;
-    }else{
-      B2->change = 1;
-      B2->blocked = 0;
-    }
-    pthread_mutex_unlock(&mutex_lockA);
-    usleep(delayB/2);
-    pthread_mutex_lock(&mutex_lockA);
-    if(N2->Adr.S == 0){
-      i2++;
-    }else{
-      B2 = N2;
-      i2 = 0;
-    }
-  }
-}
-*/

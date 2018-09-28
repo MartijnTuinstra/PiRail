276fe27326d971c3746c815e4dd948319aab75d5
MartijnTuinstra <md.tuinstra@gmail.com>
2018-09-28 13:19:01 +0200

    Exit after wrong module number

--- src/config_reader.c
+++ src/config_reader.c
@@ -821,8 +821,10 @@ int main(){
   if(!fp){
     loggerf(ERROR, "Failed to open file");
 
-    if(file <= 0 || file > 254)
+    if(file <= 0 || file > 254){
       loggerf(ERROR, "Only module numbers between 1-254 supportede");
+      return -1;
+    }
 
     loggerf(INFO,  "Creating New File");
     

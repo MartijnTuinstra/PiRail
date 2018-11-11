7505da5da0ce2c6fd354735447699fe826ac4e04
MartijnTuinstra <md.tuinstra@gmail.com>
2018-11-11 23:23:26 +0100

    Started working on Z21 implementation

--- lib/Z21.h
+++ lib/Z21.h
@@ -1,8 +1,8 @@
 
 #ifndef _INCLUDE_Z21_H
   #define _INCLUDE_Z21_H
-  #define Z21_IP "192.168.2.92"
-  #define Z21_PORT 4129
+  #define Z21_IP "192.168.2.8"
+  #define Z21_PORT 21105
 
   /*
       Simple udp client
@@ -28,14 +28,16 @@
 
   void die(char *s);
 
-  void Z21_client();
+  void Z21(pthread_t * thread);
+  int Z21_client(char * ip, uint16_t port);
+  void * Z21_run();
 
   extern char Z21_prio_list[05][30];
   extern char Z21_send_list[10][30];
 
-  void Z21_recv(char message[100]);
+  void Z21_recv(char * data, int length);
 
-  void Z21_send(int Header,char data[30]);
+  void Z21_send(uint16_t length, uint16_t header, ...);
 
   void Z21_get_train(Trains * T);
   void Z21_get_engine(int dcc);
@@ -43,4 +45,44 @@
   void Z21_SET_LOCO_DRIVE(int DCC_Adr,char steps,_Bool dir,char drive);
 
   void Z21_SET_LOCO_FUNCTION(int DCC_Adr,char function_nr,char switch_type);
+
+
+  // Z21 Commands
+  // 2 - System, status, version
+  #define Z21_GET_SERIAL                  Z21_send(4, 0x10)                                    // 2.1  - LAN_GET_SERIAL_NUMBER
+  #define Z21_LOGOUT                      Z21_send(4, 0x30)                                    // 2.2  - LAN_LOGOUT
+  #define Z21_GET_VERSION                 Z21_send(7, 0x40, 0x21, 0x21, 0x00)                  // 2.3  - LAN_X_GET_VERSION
+  #define Z21_GET_STATUS                  Z21_send(7, 0x40, 0x21, 0x24, 0x05)                  // 2.4  - LAN_X_GET_STATUS
+  #define Z21_TRACKPOWER_OFF              Z21_send(7, 0x40, 0x21, 0x80, 0xA1)                  // 2.5  - LAN_X_SET_TRACK_POWER_OFF
+  #define Z21_TRACKPOWER_ON               Z21_send(7, 0x40, 0x21, 0x81, 0xA0)                  // 2.6  - LAN_X_SET_TRACK_POWER_ON
+  #define Z21_STOP                        Z21_send(6, 0x40, 0x80, 0x80)                        // 2.13 - LAN_X_SET_STOP
+  #define Z21_GET_FIRMWARE_VERSION        Z21_send(7, 0x40, 0xF1, 0x0A, 0xFB)                  // 2.15 - LAN_X_GET_FIRMARE_VERSION
+  #define Z21_SET_BROADCAST_FLAGS(F)      Z21_send(8, 0X50, (A & 0xFF000000) >> 24, \
+                                                   (B & 0xFF0000) >> 16, (C & 0xFF00) >> 8, \
+                                                   D & 0xFF)                                   // 2.16 - LAN_X_SET_BROADCAST_FLAGS
+  #define Z21_GET_BROADCAST_FLAGS          Z21_send(4, 0X51)                                   // 2.17 - LAN_X_GET_BROADCAST_FLAGS
+  #define Z21_SYSTEMSTATE_GETDATA          Z21_send(4, 0x85)                                   // 2.19 - LAN_SYSTEMSTATE_GETDATA
+  #define Z21_GET_HWINFO                   Z21_send(4, 0x1A)                                   // 2.20 - LAN_GET_HWINFO
+
+  // 3 - Settings
+  #define Z21_GET_LOCOMODE(Addr)          Z21_send(6, 0x60, (Addr & 0xFF00) >> 8, Addr & 0xFF) // 3.1  - LAN_GET_LOCOMODE
+  #define Z21_SET_LOCOMODE(Addr, Mode)    Z21_send(6, 0x61, (Addr & 0xFF00) >> 8, \
+                                                   Addr & 0xFF, Mode)                          // 3.2  - LAN_SET_LOCOMODE
+  #define Z21_GET_TURNOUTMODE(Addr)       Z21_send(6, 0x70, (Addr & 0xFF00) >> 8, Addr & 0xFF) // 3.3  - LAN_GET_TURNOUTMODE
+  #define Z21_SET_TURNOUTMODE(Addr, Mode) Z21_send(6, 0x71, (Addr & 0xFF00) >> 8, \
+                                                   Addr & 0xFF, Mode)                          // 3.4  - LAN_SET_TURNOUTMODE
+
+  // 4 - Drive
+  // Are not define in a macro
+
+  // 5 - Switches
+  // 6 - CV Read and Write
+  // 7 - Occupancy detectors R-BUS
+  // 8 - RailCom
+  // 9 - LocoNet
+  // Not used
+
+
+
+
 #endif
--- src/Z21.c
+++ src/Z21.c
@@ -1,25 +1,170 @@
 #include "Z21.h"
+#include "mem.h"
 #include "logger.h"
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include<pthread.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+
 void die(char *s){
-	loggerf(CRITICAL, "Crashed: %s", s);
+  loggerf(CRITICAL, "Crashed: %s", s);
+  exit(1);
 }
 
-void Z21_client(){}
+int z21_fd = -1;
+
+pthread_mutex_t z21_send_mutex;
+char * z21_send_buffer;
+
+void Z21(pthread_t * thread){
+  loggerf(INFO, "Connecting to Z21");
+  int ret = 0;
+  ret = Z21_client(Z21_IP, Z21_PORT);
+  if(ret == -2){
+    //Retry on second port
+    ret = Z21_client(Z21_IP, Z21_PORT+1);
+
+    if(ret != 1){
+      return;
+    }
+  }
+  
+  if(ret == 1){
+    loggerf(INFO, "Connected Succesfully to Z21");
+    pthread_create(thread, NULL, Z21_run, NULL);
+  }
+  else{
+    return;
+  }
+}
+
+int Z21_client(char * ip, uint16_t port){
+  loggerf(DEBUG, "Z21: Connecting %s:%d", ip, port);
+  struct sockaddr_in z21_addr;
+
+  z21_fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if(z21_fd == -1){
+    loggerf(CRITICAL, "Cannot create Socket");
+    _SYS_change(STATE_RUN | STATE_Client_Accept, 3);
+    return -1;
+  }
+  
+  struct timeval tv;
+  tv.tv_sec = 30;
+  tv.tv_usec = 0;
+  setsockopt(z21_fd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
+
+  bzero(&z21_addr, sizeof(z21_addr));
+  z21_addr.sin_addr.s_addr = inet_addr(ip);
+  z21_addr.sin_port = htons(port);
+  z21_addr.sin_family = AF_INET;
+
+  if( connect(z21_fd, (void *)&z21_addr, sizeof(z21_addr)) < 0){
+    loggerf(ERROR, "UDP Connect faild");
+    close(z21_fd);
+    return -2;
+  }
+
+  char * z21_buf = _calloc(1, 1024);
+  z21_send_buffer = _calloc(1, 1024);
+  int z21_buf_size = 1024;
+
+  Z21_GET_SERIAL;
+
+  int read_length = read(z21_fd, z21_buf, z21_buf_size);
+  loggerf(DEBUG, "Z21: got serial, %d", read_length);
+  if(read_length <= 0){
+    // Failed to read data
+    return -2;
+  }
+
+  _free(z21_buf);
+  return 1;
+}
+
+void * Z21_run(){
+  char * z21_buf = _calloc(1, 1024);
+  int z21_buf_size = 1024;
+
+  while(_SYS->_STATE & STATE_RUN){
+    Z21_recv(z21_buf, read(z21_fd, z21_buf, z21_buf_size));
+  }
+
+  Z21_LOGOUT;
+    
+  _free(z21_buf);
+  close(z21_fd);
+  return NULL;
+}
 
 char Z21_prio_list[05][30];
 char Z21_send_list[10][30];
 
-void Z21_recv(char message[100]){
-	loggerf(CRITICAL, "UNIMPLEMENTED (%s)", message);
+void Z21_recv(char * data, int length){
+  // TODO Implement
+  char * sdata = _calloc(3, length);
+  for(int i = 0; i < length*3; i++){
+    switch(i % 3){
+      case 0: 
+        if((data[i/3] >> 4) < 0xA){
+          sdata[i] = (data[i/3] >> 4) + 0x30;
+        }
+        else{
+          sdata[i] = (data[i/3] >> 4) + 0x37;
+        }
+        break;
+      case 1:
+        if((data[i/3] >> 4) < 0xA){
+          sdata[i] = (data[i/3] & 0x0F) + 0x30;
+        }
+        else{
+          sdata[i] = (data[i/3] & 0x0F) + 0x37;
+        }
+        break;
+      case 2:
+        if(i != (length * 3) - 1){
+          sdata[i] = ' ';
+        }
+        break;
+    }
+  }
+  loggerf(INFO, "Z21 got data %s", sdata);
 }
 
-void Z21_send(int Header,char data[30]){
-	loggerf(CRITICAL, "UNIMPLEMENTED (%i, %s)", Header, data);
+void Z21_send(uint16_t length, uint16_t header, ...){
+  // TODO lock z21 mutex
+  z21_send_buffer[0] = length & 0x00ff;
+  z21_send_buffer[1] = (length & 0xff00) >> 8;
+  z21_send_buffer[2] = header & 0x00ff;
+  z21_send_buffer[3] = (header & 0xff00) >> 8;
+  if(length != 4){
+    va_list arglist;
+    va_start(arglist, header);
+
+    for(int i = 4; i<length; i++){
+      z21_send_buffer[i] = (uint8_t)va_arg(arglist, int);
+    }
+
+    va_end(arglist);
+  }
+  
+  write(z21_fd, z21_send_buffer, length);
+  // TODO unlock z21 mutex
 }
 
 void Z21_get_train(Trains * T){
   loggerf(ERROR, "Implement Z21_get_train (%x)", (unsigned int)T);
+  Z21_LOGOUT;
+  Z21_GET_SERIAL;
+  Z21_GET_STATUS;
+  Z21_TRACKPOWER_ON;
+  Z21_TRACKPOWER_OFF;
+  Z21_STOP;
+  Z21_GET_FIRMWARE_VERSION;
 }
 
 void Z21_get_engine(int dcc){

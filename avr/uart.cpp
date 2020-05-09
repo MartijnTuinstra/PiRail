56d747e01f62b3e2438e78b38129bb44c5c3a570
M.D. Tuinstra <md.tuinstra@gmail.com>
2020-05-09 16:25:09 +0200

    replaced tabs by spaces avr/uart.cpp

--- avr/uart.cpp
+++ avr/uart.cpp
@@ -62,27 +62,27 @@ void UART::init(){
 }
 
 // bool UART::available(){
-// 	return (UCSR0A & (1<<RXC0));
+//  return (UCSR0A & (1<<RXC0));
 // }
 
 uint8_t UART::receive(){
-	while (!available()){}
+  while (!available()){}
 
-	return UDR0;
+  return UDR0;
 }
 
 void UART::_rx_complete_irq(){
-	net.tx.buf[net.tx.write_index] = UDR0;
+  net.tx.buf[net.tx.write_index] = UDR0;
 
-	if(++net.tx.write_index >= RNET_MAX_BUFFER){
+  if(++net.tx.write_index >= RNET_MAX_BUFFER){
     net.tx.write_index = 0;
   }
 }
 
 void UART::start_tx(){
-	UDR0 = net.rx.buf[net.rx.read_index];
+  UDR0 = net.rx.buf[net.rx.read_index];
 
-	if(++net.rx.read_index >= RNET_MAX_BUFFER){
+  if(++net.rx.read_index >= RNET_MAX_BUFFER){
     net.rx.read_index = 0;
   }
 
@@ -90,91 +90,91 @@ void UART::start_tx(){
 }
 
 void UART::_tx_complete_irq(){
-	#ifdef RNET_MASTER
-    if(net.rx.read_index != net.rx.write_index){
-		UDR0 = net.rx.buf[net.rx.read_index];
+  #ifdef RNET_MASTER
+  if(net.rx.read_index != net.rx.write_index){
+    UDR0 = net.rx.buf[net.rx.read_index];
 
-		if(++net.rx.read_index >= RNET_MAX_BUFFER){
-          net.rx.read_index = 0;
-        }
+    if(++net.rx.read_index >= RNET_MAX_BUFFER){
+      net.rx.read_index = 0;
+    }
 
-    	UCSR0B |= (1<<UDRIE0);
-    	UCSR0A &= ~(1<<UDRE0); // Clear interrupt
-    }
-    else{
-    	UCSR0B &= ~(1<<UDRIE0);
-    }
-	#endif
+    UCSR0B |= (1<<UDRIE0);
+    UCSR0A &= ~(1<<UDRE0); // Clear interrupt
+  }
+  else{
+    UCSR0B &= ~(1<<UDRIE0);
+  }
+  #endif
 }
 
 uint8_t UART::available(){
-	return (w != r);
+  return (w != r);
 }
 
 char UART::read(){
-	uint8_t d = buf[r++];
-	if(r > UART_BUF_SIZE){
-		r = 0;
-	}
-	return d;
+  uint8_t d = buf[r++];
+  if(r > UART_BUF_SIZE){
+    r = 0;
+  }
+  return d;
 }
 
 void UART::transmit(uint8_t byte){
-    loop_until_bit_is_set(UCSR0A, UDRE0); /* Wait until data register empty. */
-    UDR0 = byte;
+  loop_until_bit_is_set(UCSR0A, UDRE0); /* Wait until data register empty. */
+  UDR0 = byte;
 }
 void UART::transmit(uint8_t * data, uint8_t length){
-	for(uint8_t i = 0; i < length; i++){
-		transmit(data[i]);
-	}
+  for(uint8_t i = 0; i < length; i++){
+    transmit(data[i]);
+  }
 }
 void UART::transmit(const char * data, uint8_t length){
-	for(uint8_t i = 0; i < length; i++){
-		transmit(data[i]);
-	}
+  for(uint8_t i = 0; i < length; i++){
+    transmit(data[i]);
+  }
 }
 
 void UART::transmit(long q, enum UART_transmit_type t){
-	char buf[5] = {0,0,0,0,0};
-	if(t == HEX){
-		uint8_t i = 0;
-		while(q != 0){
-			long r = q % 16;
-			if(r < 10)
-				buf[i] = (r + 48);
-			else
-				buf[i] = (r + 55);
-			i++;
-			q = q / 16;
-		}
+  char buf[5] = {0,0,0,0,0};
+  if(t == HEX){
+    uint8_t i = 0;
+    while(q != 0){
+      long r = q % 16;
+      if(r < 10)
+        buf[i] = (r + 48);
+      else
+        buf[i] = (r + 55);
+      i++;
+      q = q / 16;
+    }
 
-		for(int8_t j = i - 1; j >= 0; j--){
-			uart.transmit(buf[j]);
-		}
-	}
+    for(int8_t j = i - 1; j >= 0; j--){
+      uart.transmit(buf[j]);
+    }
+  }
 }
 
 
 void UART::transmit(long q, enum UART_transmit_type t, uint8_t length){
-	char buf[5] = {0,0,0,0,0};
-	if(t == HEX){
-		uint8_t i = 0;
-		while(q != 0){
-			long r = q % 16;
-			if(r < 10)
-				buf[i] = (r + 48);
-			else
-				buf[i] = (r + 55);
-			i++;
-			q = q / 16;
-		}
+  char buf[5] = {0,0,0,0,0};
+  if(t == HEX){
+    uint8_t i = 0;
+    while(q != 0){
+      long r = q % 16;
+      if(r < 10)
+        buf[i] = (r + 48);
+      else
+        buf[i] = (r + 55);
+      i++;
+      q = q / 16;
+    }
 
-		for(uint8_t j = 0; j < length - i; j++){
-			uart.transmit('0');
-		}
+    for(uint8_t j = 0; j < length - i; j++){
+      uart.transmit('0');
+    }
 
-		for(int8_t j = i - 1; j >= 0; j--){
-			uart.transmit(buf[j]);
-		}
-	}
+    for(int8_t j = i - 1; j >= 0; j--){
+      uart.transmit(buf[j]);
+    }
+  }
 }

8286cb23aafd8ce98a1624c2e84fe01bd96f1b5c
Martijn Tuinstra <md.tuinstra@gmail.com>
2020-08-30 15:28:56 +0200

    added write_from_filedescriptor

--- shared/lib/circularbuffer.h
+++ shared/lib/circularbuffer.h
@@ -4,13 +4,21 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#ifdef CB_NON_AVR
+#define CB_MAX_BUFFER 1024
+#else
 #define CB_MAX_BUFFER 256
+#endif
 
 class CircularBuffer {
   public:
     uint8_t buf[CB_MAX_BUFFER];
     uint8_t read_index;
     uint8_t write_index;
+
+    #ifdef CB_NON_AVR
+    uint8_t writefromfd(int fd);
+    #endif
 
     uint8_t read();
     void write(uint8_t data);
--- shared/src/circularbuffer.cpp
+++ shared/src/circularbuffer.cpp
@@ -1,6 +1,35 @@
 
 #include "circularbuffer.h"
 
+
+#ifdef CB_NON_AVR
+uint8_t writefromfd(int fd){
+  if(fd < 0)
+    return;
+
+  //Filestream, buffer to store in, number of bytes to read (max)
+  uint8_t size = 32;
+  if(write_index + size > CB_MAX_BUFFER)
+    size = CB_MAX_BUFFER - write_index;
+
+  int rx_length = read(fd, (void*)&buf[write_index], size);
+  if (rx_length > 0)
+  {
+    char debug[200];
+    char *ptr = debug;
+
+    for(uint8_t i = 0;i<rx_length;i++){
+      ptr += sprintf(ptr, "%02x", buf[i+write_index]);
+    }
+
+    write_index = (write_index + rx_length) % CB_MAX_BUFFER;
+
+    // loggerf(INFO, "%i/%i-(%i/%i) bytes read, %d available, %s", rx_length, size, buf->read, buf->write, (uint8_t)(((int16_t)buf->write - (int16_t)buf->read) % UART_BUFFER_SIZE), debug);
+  }
+  return rx_length;
+}
+#endif
+
 uint8_t CircularBuffer::read(){
   if(read_index != write_index){
     uint8_t byte = buf[read_index];
